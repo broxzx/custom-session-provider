46dae7c43c0d35ee462fd87549e0a4b5f27a8f9b
Ilya_Sedelnikov <illyasedelnikov123@gmail.com>
2024-05-27 11:37:53 +0200

    custom session provider

--- src/main/java/com/project/provider/CustomEventListenerProvider.java
+++ src/main/java/com/project/provider/CustomEventListenerProvider.java
@@ -1,2 +1,57 @@
-package com.project.provider;public class CustomEventListenerProvider {
+package com.project.provider;
+
+import org.keycloak.events.Event;
+import org.keycloak.events.EventListenerProvider;
+import org.keycloak.events.EventType;
+import org.keycloak.events.admin.AdminEvent;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.UserSessionModel;
+
+import java.util.List;
+
+public class CustomEventListenerProvider implements EventListenerProvider {
+
+    private final KeycloakSession session;
+
+
+    public CustomEventListenerProvider(KeycloakSession session) {
+        this.session = session;
+    }
+
+
+    @Override
+    public void onEvent(Event event) {
+        if (event.getType() == EventType.LOGIN) {
+            System.out.println("Login event detected for user: " + event.getUserId());
+
+            String userId = event.getUserId();
+            List<UserSessionModel> userSessions = session.sessions().getUserSessionsStream(session.getContext().getRealm(),
+                            session.users().getUserById(session.getContext().getRealm(), userId))
+                    .toList();
+
+            System.out.println("userSessions: " + userSessions.size());
+            if (userSessions.size() >= 2) {
+                UserSessionModel oldestSession = userSessions.get(0);
+                for (UserSessionModel userSession : userSessions) {
+                    if (userSession.getStarted() < oldestSession.getStarted()) {
+                        oldestSession = userSession;
+                    }
+                }
+
+
+                session.sessions().removeUserSession(session.getContext().getRealm(), oldestSession);
+                System.out.println("Oldest session removed for user: " + userId);
+            }
+        }
+    }
+
+    @Override
+    public void onEvent(AdminEvent adminEvent, boolean b) {
+
+    }
+
+    @Override
+    public void close() {
+
+    }
 }
--- src/main/java/com/project/providerFactory/CustomEventListenerProviderFactory.java
+++ src/main/java/com/project/providerFactory/CustomEventListenerProviderFactory.java
@@ -1,2 +1,33 @@
-package com.project.providerFactory;public class CustomEventListenerProviderFactory {
+package com.project.providerFactory;
+
+import com.project.provider.CustomEventListenerProvider;
+import org.keycloak.events.EventListenerProvider;
+import org.keycloak.events.EventListenerProviderFactory;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+
+public class CustomEventListenerProviderFactory implements EventListenerProviderFactory {
+
+    @Override
+    public EventListenerProvider create(KeycloakSession session) {
+        return new CustomEventListenerProvider(session);
+    }
+
+    @Override
+    public void init(org.keycloak.Config.Scope config) {
+    }
+
+    @Override
+    public void postInit(KeycloakSessionFactory factory) {
+    }
+
+    @Override
+    public void close() {
+    }
+
+    @Override
+    public String getId() {
+        return "custom-event-listener";
+    }
+
 }
